20a5325a9d55458e4740e940b8150aed214991eb
unknown <josellerenasm@gmail.com>
2022-08-31 15:02:48 -0500

    Finished the app. Some CSS changes and private data protection

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ var inputCityName = document.querySelector('#input-city');
 var labelCityName = document.querySelector('#city-name');
 var tempDescription = document.querySelector('#temperature-description');
 var tempDegree = document.querySelector('#temperature-degree');
+var tempDesc = "";
 
 // REPLACE 'MY-API-KEY' WITH MY KEY. IT'S IN OPENWEATHERMAP.COM, AND INSIDE AN EMAIL THEY SEND ME
 
 btSubmit.addEventListener('click', function(){
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputCityName.value + '&APPID=MY-API-KEY')
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputCityName.value + '&APPID=' + MY_API_KEY)
   .then(response => response.json())
   .then(data => {
     labelCityName.innerHTML = data['name'];
     tempDegree.innerHTML = Math.floor(data['main']['temp'] - 273.15);
-    tempDescription.innerHTML = data['weather'][0]['description'];
+    tempDesc = data['weather'][0]['description'];
+    tempDescription.innerHTML = tempDesc.charAt(0).toUpperCase() + tempDesc.slice(1);
+    inputCityName.value = "";
   })
   .catch(err => alert("Wrong city name!"))
 });
